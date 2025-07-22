6ab8864a1bd475f2390a509f1ac56303b6076a1a
XhiroYui <xhiro.yui@gmail.com>
2025-07-22 12:28:55 +0800

    Some layout changes to look nicer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
     <div className="w-full px-4 sm:px-6 lg:px-12 xl:px-20 pt-6">
-        <h1 className="text-3xl font-bold text-blue-900 mb-4 px-2">MaiMai D-Reader</h1>
+        <h1 className="text-3xl font-bold mb-4 px-2 text-[var(--color-header)]">MaiMai D-Reader</h1>
     </div>
 )
 export default Header
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import type {ReactNode} from "react"
 
 export default function Layout({children}: { children: ReactNode }) {
     return (
-        <div className="min-h-screen bg-[var(--color-bg)] text-[var(--color-text)] transition-colors duration-300">
+        <div className="min-h-screen bg-[var(--color-bg)] text-[var(--color-text)] transition-colors duration-300 p-2">
             <div className="relative min-h-screen">
                 {children}
             </div>
